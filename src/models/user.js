128f41b3b48b471c6196c4aa584c31bc4aee4857
takumab <takumab@gmail.com>
2019-03-23 14:59:31 +0000

    Add user attrs to model - Add validator package to validate use email - Add router for user

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,34 @@
+const mongoose = require('mongoose')
 const { Schema } = mongoose;
+const validator = require('validator')
 
-const User = new Schema({
 
+/*
+  User
+  -----
+    * name 
+    * email
+    * password
+*/
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid')
+      }
+    }
+  }
 })
+
+const User = mongoose.model('User', UserSchema)
 
 module.exports = User
