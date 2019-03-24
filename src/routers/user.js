26297618f89e99c4d78430665d13f9c2a623b3bc
takumab <takumab@gmail.com>
2019-03-24 11:05:30 +0000

    Add user update route

--- src/routers/user.js
+++ src/routers/user.js
@@ -65,6 +65,38 @@ router.get('/users/:id', async (req, res) => {
 
 })
 
+router.patch('/users/:id', async (req, res) => {
+  const _id              = req.params.id 
+  const updates          = Object.keys(req.body)
+  const allowedUpdates   = ['name', 'email', 'password']
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates' })
+  }
+
+  try {
+    const user = await User.findById(_id)
+
+    updates.forEach((update) => user[update] = req.body[update])
+    await user.save()
+
+    if (!user) {
+      return res.status(404).send()
+    }
+
+    res.send(user)
+
+
+  } catch (e) {
+
+    res.status(400).send(e)
+
+  }
+})
+
+router.delete('/users/:id')
+
 
 
 
