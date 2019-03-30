430804eb7863c34e5b9981834c5bb7fa4b64ba30
takumab <takumab@gmail.com>
2019-03-30 07:55:32 +0000

    Tidy up code

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose')
+const mongoose   = require('mongoose')
 const { Schema } = mongoose;
-const validator = require('validator')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const validator  = require('validator')
+const jwt        = require('jsonwebtoken')
+const bcrypt     = require('bcrypt')
 
 
 /*
